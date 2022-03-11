01e3a749631cdd3aff04cbdc77c03b5621521833
Sarp Koray Karakaya <74262892+Sarpheim@users.noreply.github.com>
2022-03-11 18:30:58 +0300

    Update tdk-word-search-function.js

--- tdk-word-search-function.js
+++ tdk-word-search-function.js
@@ -1,3 +1,5 @@
-module.exports = async (fetch, src) => {
+module.exports = async (src) => {
+  const fetch = require("node-fetch")
+  
   return await fetch(`https://sozluk.gov.tr/gts?ara=${encodeURI(src.toLowerCase())}`).then((response) => response.json())
 }
